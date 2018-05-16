8958c168c373ba53a22d81b6e7ca0b0089ed694d
Artsiom Kiseliou <akr1996@mail.ru>
2018-05-16 23:04:18 +0300

    lower

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectID=require('mongodb').ObjectID;
+var cors = require('cors');
 
 var app = express();
 var db = require('./db');
@@ -11,6 +12,14 @@ var usersController=require('./controllers/users');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.all(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var exhibitions = [];
 var exponats = [];
